6b1cb4399b2ab4ad0be187f578b4fe40ac8f70bf
yuenicky <60953907+yuenicky@users.noreply.github.com>
2020-02-14 13:50:15 +0800

    my instroduction
    some cpps of mine

---
+++ Nicky.cpp
@@ -0,0 +1,21 @@
+/*author:Nicky yue
+ *file name:Nicky
+ *function:my introduction of my self
+ *date:2/14/2020
+ */
+
+ #include<iostream>
+ using namespace std;
+ int main()
+ {
+     string m;
+     cout<<"input your name"<<endl;
+     cin>>m;
+     cout << "yes,thank you "<< m <<endl;
+     cout << "input your interest please"<<endl;
+     string n;
+     cin>>n;
+     cout << "you like "<<n<<" the most"<<endl;
+     return 0;
+ }
+
